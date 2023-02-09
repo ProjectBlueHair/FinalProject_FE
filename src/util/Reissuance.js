d2546188791d8060af72c408a03be092c4406cd1
mugyeol <htpadkorik@naver.com>
2023-02-09 21:30:10 +0900

    [#181] refactor : setTotal page 프로필 편집 후 토큰 재발행 시 onIssued 함수 추가

--- src/util/Reissuance.js
+++ src/util/Reissuance.js
@@ -1,5 +1,5 @@
 import { instanceAxios } from "../dataManager/apiConfig";
-import { getCookies } from "../dataManager/cookie";
+import { getCookies, removeCookies, setCookie } from "../dataManager/cookie";
 
 export const reissuance = () => {
   const RefreshToken = getCookies("refreshtoken");
@@ -15,3 +15,20 @@ export const reissuance = () => {
     }
   );
 };
+//닉네임 변경 시 토큰 재발행 위해서 작성
+export const onIssued = (data) => {
+  const { accesstoken, refreshtoken } = data.headers;
+  if (!accesstoken || !refreshtoken) {
+    removeCookies("accesstoken", { path: "/" });
+    removeCookies("refreshtoken", { path: "/" });
+    throw new Error(
+      "로그인이 만료되었습니다. 다시 로그인 해주세요"
+    );
+  }
+  setCookie("accesstoken", accesstoken, {
+    path: "/",
+  });
+  setCookie("refreshtoken", refreshtoken, {
+    path: "/",
+  });
+}
